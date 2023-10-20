1ff82f8a440d34f66dae8a6f19de2a114ab983b2
MariaMantusova <maria.mantusova@mail.ru>
2023-10-20 16:37:12 +0300

    feat: add styles

--- src/app/recipes/[id]/error.tsx
+++ src/app/recipes/[id]/error.tsx
@@ -3,6 +3,6 @@ import { MessageBlock } from "../../../components/MessageBlock/MessageBlock";
 
 export default function Error(error) {
   return (
-    <MessageBlock text={error.message || "Ooops... error has happened"} class="message-block__text_error"/>
+    <MessageBlock text={error.message || "Ooops... error has happened"} class="message-block__text_error message-block__page"/>
   )
 }
