861eb44b11d9a507c692708197f9e17aec00fb8c
MariaMantusova <maria.mantusova@mail.ru>
2023-10-20 16:49:34 +0300

    feat: add error type

--- src/app/recipes/[id]/error.tsx
+++ src/app/recipes/[id]/error.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { MessageBlock } from "../../../components/MessageBlock/MessageBlock";
 
-export default function Error(error) {
+export default function Error({ error }: { error: Error }) {
   return (
     <MessageBlock text={error.message || "Ooops... error has happened"} class="message-block__text_error message-block__page"/>
   )
